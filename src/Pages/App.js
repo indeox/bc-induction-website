0a154cde9d18e558a64ddd89682d69afc896e7bd
chrismphilp <chrismphilp@hotmail.co.uk>
2017-07-28 11:13:07 +0100

    Adding parallax

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -9,7 +9,6 @@ export default class App extends Component {
 		  	<div>
 			  	<MenuBar />
 			  	<PageLayout />
-			  	<Footer />
 		  	</div>
 		);
 	}
--- src/Pages/QuizPage.js
+++ src/Pages/QuizPage.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import questions from '../Components/QuizQuestions.js';
+import Parallax from 'react-springy-parallax';
+
+import history from '../Images/history-landing-page.jpg';
+import greatd from '../Images/thegreatd.jpg';
+import greatd2 from '../Images/greatd2.jpg';
+import modern_bc from '../Images/modern_bc.jpg';
+import liberation from '../Images/liberation.jpg';
 
 export default class QuizPage extends Component {
 
 	render() {
 		return (
 			<div>
-				
+				<Parallax ref='parallax' pages={3}>
+				    <Parallax.Layer offset={0} speed={0.5} >
+				        <span><img src={history} height='100%' width='100%' alt='#' /></span>
+				    </Parallax.Layer>
+				    <Parallax.Layer offset={0.99} speed={0.65}>
+				        <span><img src={greatd} height='100%' width='100%' alt='#' /></span>
+				    </Parallax.Layer>
+				    <Parallax.Layer offset={1.25} speed={0.5}>
+				        <span><img src={greatd2} height='100%' width='100%' alt='#' /></span>
+				    </Parallax.Layer>
+				    <Parallax.Layer offset={1.9} speed={0.1}>
+				        <span><img src={liberation} height='100%' width='100%' alt='#' /></span>
+				    </Parallax.Layer>
+				    <Parallax.Layer offset={2.00} speed={1.75}>
+				        <span><img src={modern_bc} height='100%' width='100%' alt='#' /></span>
+				    </Parallax.Layer>
+				</Parallax>
 			</div>
 		);
 	}
