fa6a86afba46a279eda053f19f6f10200358ee27
chrismphilp <chrismphilp@hotmail.co.uk>
2017-08-10 15:56:23 +0100

    Text boxes bois

--- src/Pages/QuizPage.js
+++ src/Pages/QuizPage.js
@@ -47,28 +47,67 @@ export default class QuizPage extends Component {
 
 					{/* Page 1 */}
 				    <Parallax.Layer offset={0} speed={0.5} onClick={() => this.refs.parallax.scrollTo(0.6)} >
-				        <span><img src={history} height='100%' width='100%' alt='#' /></span>
+				        <div className="slide" id='slide1'>
+						  	<div className="title">
+						    	<h1>Our humble beginnings</h1>
+						    	The early 1930s was a time of growing global instability and there were increasing 
+						    	threats to British prosperity, security and influence.  The Wall Street Crash of 1929 
+						    	led to the Great Depression of the early 1930s with a huge drop in international trade, 
+						    	falls in living standards, and high and persistent unemployment.
+						  	</div>
+						</div>
 				    </Parallax.Layer>
 				    {/* Page 2 */}
 				    <Parallax.Layer offset={0.99} speed={0.65} onClick={() => this.refs.parallax.scrollTo(1.2)} >
-				        <span><img src={greatd} height='100%' width='100%' alt='#' /></span>
+				        <div className="slide" id='slide2'>
+						  	<div className="title">
+						    	<h1>Growing tensions...</h1>
+						    	The balance of power was shifting and extreme ideologies were gaining ground. The October 
+						    	Revolution of 1917 had brought the Bolsheviks to power in Russia, while in 1922 Mussolini 
+						    	had taken charge of Italy. This was followed by the rise of Nazism in Germany, with Hitler 
+						    	coming to power in Berlin in January 1933. In Spain, the civil war that would bring Franco’s 
+						    	Falangists to power was shortly to begin. Across Europe and the wider world, Britain’s influence 
+						    	in trade and diplomacy was under significant challenge.
+						  	</div>
+						</div>
 				    </Parallax.Layer>
 				    {/* Page 3 */}
-				    <Parallax.Layer offset={1.25} speed={0.5} onClick={() => this.refs.parallax.scrollTo(1.82)}>
-				        <span><img src={greatd2} height='100%' width='100%' alt='#' /></span>
+				    <Parallax.Layer offset={1.35} speed={0.5} onClick={() => this.refs.parallax.scrollTo(1.82)}>
+				        <div className="slide" id='slide3'>
+						  	<div className="title">
+						    	<h1>Growing tensions...</h1>
+						    	One of the ways that the government reacted to the growing threat to British interests was the 
+						    	creation of the British Council. It began operations in 1934.  The organisation’s original aim 
+						    	is set out in this extract from our 1940-41 Annual Report:
+
+								“The Council’s aim is to create in a country overseas a basis of friendly knowledge and understanding 
+								of the people of this country, of their philosophy and way of life, which will lead to a sympathetic 
+								appreciation of British foreign policy, whatever for the moment that policy may be and from whatever 
+								political conviction it may spring. While in times of danger this friendly knowledge and understanding 
+								becomes vital to the successful prosecution of war (that is the Council’s place in the war effort), in 
+								times of peace it is not less valuable".
+						  	</div>
+						</div>
 				    </Parallax.Layer>
 				    {/* Page 4 */}
-				    <Parallax.Layer offset={1.9} speed={0.1} onClick={() => this.refs.parallax.scrollTo(2.54)} >
-				        <span><img src={liberation} height='100%' width='100%' alt='#' /></span>
+				    <Parallax.Layer offset={2} speed={0.35} onClick={() => this.refs.parallax.scrollTo(2.5)} >
+				        <div className="slide" id='slide4'>
+						  	<div className="title">
+						    	<h1>Royal Charter Bois</h1>
+								Our mission in the Royal Charter was stated as: “promoting a wider knowledge of [the United Kingdom] 
+								and the English language abroad and developing closer cultural relations between [the UK] and other 
+								countries”.
+						  	</div>
+						</div>
 				    </Parallax.Layer>
 					{/* Page 5 */}
-				    <Parallax.Layer offset={2.7} speed={0.2} style={style1} >
+				    <Parallax.Layer offset={2.7} speed={0.} style={style1} >
 				    		<span><img src={uruguay} alt='#' height='125%' width='100%' /></span>
 				    </Parallax.Layer>
-				    <Parallax.Layer offset={2.7} speed={0.2} style={style2} >
+				    <Parallax.Layer offset={2.7} speed={0.1} style={style2} >
 				    		<img src={classroom} alt='#' height='52.5%' width='29.5%' />
 				    </Parallax.Layer>
-				    <Parallax.Layer offset={2.7} speed={0.2} style={style3} onClick={() => this.refs.parallax.scrollTo(3.3)} >
+				    <Parallax.Layer offset={2.7} speed={0.2} style={style3} onClick={() => this.refs.parallax.scrollTo(3)}>
 				    		<img src={teaching} alt='#' height='52.5%' width='35%' />
 				    </Parallax.Layer>
 				    <Parallax.Layer offset={3.1} speed={0.2} style={style2} ><p>Our work in</p></Parallax.Layer>
