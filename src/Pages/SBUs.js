37160689bb074787c24e422b08b7ac828f970d6a
kmc26 <kristen.carter@britishcouncil.org>
2017-08-15 11:57:14 +0100

    Added SBU page

--- src/Pages/SBUs.js
+++ src/Pages/SBUs.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import ProgressBar from "../Components/ProgressBar.js";
 import logo from "../Images/British-Council.jpg";
 
-export default class OurBehaviours extends Component {
+export default class SBUs extends Component {
   render() {
     return (
       <div>
         <ProgressBar
-          continueLink={"#/SBUs"}
+          continueLink={"#/NeedToKnow"}
           stepLabels={[
             "History Shite",
             "Value Shite",
@@ -15,7 +15,7 @@ export default class OurBehaviours extends Component {
             "SBU Shite",
             "Need-to-Know Shite"
           ]}
-          currentIndex={2}
+          currentIndex={3}
         />
       </div>
     );
