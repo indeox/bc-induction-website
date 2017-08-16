fd05369acb136612bba083431dc586c5d2e069ff
chrismphilp <chrismphilp@hotmail.co.uk>
2017-08-16 12:12:22 +0100

    Compeltion page

---
+++ src/Pages/Completion.js
@@ -0,0 +1,32 @@
+import Parallax from 'react-springy-parallax';
+import React, { Component } from 'react';
+
+import Footer from '../Components/Footer';
+import ProgressBar from '../Components/ProgressBar';
+
+export default class About extends Component {
+
+	render() {
+		return (
+			<div>
+				<Parallax ref='parallax' pages={1}>
+					<div className="container">
+						<ProgressBar 
+							continueLink={"#/Completion"}
+							currentIndex={5}
+		          			stepLabels={[
+					            "Our History",
+					            "Our Values",
+					            "Our Behaviours",
+					            "Our SBU's",
+					            "Key Information",
+					            "Finish Up"
+		          			]}
+		          		/>
+		          		
+		          	</div>
+				</Parallax>
+			</div>
+		);
+	}
+}
