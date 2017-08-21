ce099defa880e63882b6eae6ea45bd71322a2424
kmc26 <kristen.carter@britishcouncil.org>
2017-08-21 17:32:01 +0100

    sorted transition, need to fill in SBU info

--- src/Pages/SBUs.js
+++ src/Pages/SBUs.js
@@ -3,27 +3,29 @@ import Parallax from "react-springy-parallax";
 
 import ProgressBar from "../Components/ProgressBar.js";
 import PinnedMap from "../Components/PinnedMap.js";
-import SwipeWrapper from "../Components/SwipeWrapper.js";
+import TwoPageTransition from "../Components/TwoPageTransition.js";
 
 export default class OurBehaviours extends Component {
-  	render() {
-    	return (
-	      	<div>
-		        <Parallax ref="parallax" pages={1.2}>
-		        	<ProgressBar 
-		        		continueLink={"#/NeedToKnow"}
-						currentIndex={3}
-				        stepLabels={[
-				            "Our History",
-				            "Our Values",
-				            "Our Behaviours",
-				            "Our SBU's",
-				            "Key Information"
-		      			]}
-		        	/>
-		          	<SwipeWrapper />
-		        </Parallax>
-	      	</div>
-    	);
-  	}
+  render() {
+    return (
+      <div>
+        <Parallax ref="parallax" pages={1.2}>
+          <ProgressBar
+            continueLink={"#/NeedToKnow"}
+            currentIndex={3}
+            stepLabels={[
+              "Our History",
+              "Our Values",
+              "Our Behaviours",
+              "Our SBU's",
+              "Key Information"
+            ]}
+          />
+          <TwoPageTransition
+            map={<PinnedMap />}
+          />
+        </Parallax>
+      </div>
+    );
+  }
 }
